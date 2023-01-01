66c0db6621e4f74d073fc9b27a67c74b3a64228a
Ridwan Olanrewaju <rolanrewaju@afexnigeria.com>
2023-01-01 02:28:17 +0100

    adjustments to offset calculations

--- pages/portfolio/the-residences.tsx
+++ pages/portfolio/the-residences.tsx
@@ -1,6 +1,6 @@
 import { ActionIcon } from "@mantine/core";
 import { ArrowLeft } from "iconsax-react";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { css } from "@emotion/css";
 
 import Link from "next/link";
@@ -14,46 +14,43 @@ import {
 } from "@/layouts/portfolio/property";
 
 export default function TheResidencesPage() {
-  const ref = useRef<HTMLElement>();
-  const containerRef = useRef<HTMLDivElement>();
+  const jumboRef = useRef<HTMLElement>();
   const linkRef = useRef<HTMLButtonElement>();
 
-  const [scroll, setScroll] = useState(0);
   const [offset, setOffset] = useState(false);
 
-  useEffect(() => {
-    if (ref.current && linkRef) {
+  function handleScroll() {
+    if (jumboRef.current && linkRef.current) {
       const { top: linkTop, bottom: linkBottom } =
         linkRef.current.getBoundingClientRect();
-      const { top, bottom } = ref.current.getBoundingClientRect();
-      setOffset(linkBottom > top && linkTop < bottom);
+
+      const { top: jumboTop, bottom: jumboBottom } =
+        jumboRef.current.getBoundingClientRect();
+
+      setOffset(linkBottom > jumboTop && linkTop < jumboBottom);
     }
-  }, [scroll]);
+  }
 
   return (
-    <div
-      onScrollCapture={() => setScroll(containerRef.current?.scrollTop)}
-      ref={containerRef}
-      className="grid h-screen overflow-auto"
-    >
+    <div onScroll={handleScroll} className="grid h-screen overflow-auto">
       <Head>
         <meta name="description" content="The Residences" />
         <title>Kappa Square | Portfolio</title>
       </Head>
 
       <article className="h-[100vh] w-full relative sm:pt-28">
-        <Landing ref={ref} />
+        <Landing ref={jumboRef} />
         <section className="absolute top-0 h-full">
           <div className="top-0 hidden pb-6 pl-10 pt-14 lg:block lg:sticky">
             <Link href="/portfolio">
               <ActionIcon
+                ref={linkRef}
                 className={css({
                   color: offset ? "white" : "black",
                   "&:hover": {
                     color: "black",
                   },
                 })}
-                ref={linkRef}
                 size="lg"
               >
                 <ArrowLeft size={30} />
